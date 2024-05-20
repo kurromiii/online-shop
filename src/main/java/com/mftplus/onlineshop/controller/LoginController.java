730f4b6339736cad0fe8b05ef25028ef0575e806
kurromii <ahadzadeparnian@gmail.com>
2024-05-20 04:03:38 +0330

    LoginController added

---
+++ src/main/java/com/mftplus/onlineshop/controller/LoginController.java
@@ -0,0 +1,53 @@
+package com.mftplus.onlineshop.controller;
+
+import com.mftplus.onlineshop.model.Role;
+import com.mftplus.onlineshop.model.User;
+import com.mftplus.onlineshop.repository.RoleRepository;
+import com.mftplus.onlineshop.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class LoginController {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    public LoginController(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository, RoleRepository roleRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerGet(){
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException {
+        String password = user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(2L).get());
+        user.setRoles(roles);
+        userRepository.save(user);
+        request.login(user.getEmail(), password);
+        return "redirect:/";
+    }
+}
